7a72b42e5e8defdae791b99c4afc181056e5e699
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-06-14 14:41:26 -0400

    Tv left to fill in

--- src/tvList.js
+++ src/tvList.js
@@ -1,8 +1,58 @@
 const tvWatched = [
     {
-        id: "",
-        title: "",
-        poster: ""
+        id: "100698",
+        title: "Tiger King: Murder, Mayhem and Madness",
+        poster: "https://image.tmdb.org/t/p/w200/pmjYMCnSwndlEpiFZhhOWSWmUsu.jpg"
+    },
+    {
+        id: "75871",
+        title: "The Toys That Made Us",
+        poster: "https://image.tmdb.org/t/p/w200/wRQe3TZdNaM2qxytMyTvTrYBaDO.jpg"
+    },
+    {
+        id: "93219",
+        title: "The Movies That Made Us",
+        poster: "https://image.tmdb.org/t/p/w200/kowlD5nELdhoJcQY6J8JZUTeNXN.jpg"
+    },
+    {
+        id: "688",
+        title: "The West Wing",
+        poster: "https://image.tmdb.org/t/p/w200/rMdRbtqJPIO6d71JD7bOwTl8105.jpg"
+    },
+    {
+        id: "74204",
+        title: "Big Mouth",
+        poster: "https://image.tmdb.org/t/p/w200/h6InpGTYbu03b6iVsnniVPqPfIw.jpg"
+    },
+    {
+        id: "2316",
+        title: "The Office",
+        poster: "https://image.tmdb.org/t/p/w200/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg"
+    },
+    {
+        id: "59659",
+        title: "Halt and Catch Fire",
+        poster: "https://image.tmdb.org/t/p/w200/l4qvJ0lq59wR3ODX42DxBVFGoxx.jpg"
+    },
+    {
+        id: "59717",
+        title: "Comedians in Cars Getting Coffee",
+        poster: "https://image.tmdb.org/t/p/w200/xvQ0jJmrYjxjPMEHP4e8IYIjojF.jpg"
+    },
+    {
+        id: "66732",
+        title: "Stranger Things",
+        poster: "https://image.tmdb.org/t/p/w200/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg"
+    },
+    {
+        id: "2693",
+        title: "Extras",
+        poster: "https://image.tmdb.org/t/p/w200/zV4Xpdkk6IrGk2mnhrKdtQOHj4I.jpg"
+    },
+    {
+        id: "88728",
+        title: "I Think You Should Leave with Tim Robinson",
+        poster: "https://image.tmdb.org/t/p/w200/m4iNYM0ZaaH10Qzebd9dwSa6Zy6.jpg"
     },
     {
         id: "",
@@ -49,23 +99,23 @@ const tvWatched = [
         title: "",
         poster: ""
     },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    }
 ]
 
 const tvWatching = [
     {
-        id: "",
-        title: "",
-        poster: ""
+        id: "18347",
+        title: "Community",
+        poster: "https://image.tmdb.org/t/p/w200/3KUjDt8XY7w2Ku70UE0SECmv1zP.jpg"
+    },
+    {
+        id: "1438",
+        title: "The Wire",
+        poster: "https://image.tmdb.org/t/p/w200/4lbclFySvugI51fwsyxBTOm4DqK.jpg"
+    },
+    {
+        id: "69740",
+        title: "Ozark",
+        poster: "https://image.tmdb.org/t/p/w200/oy7Peo5iFIt9sNM59lN6csbJeX2.jpg"
     },
     {
         id: "",
@@ -102,41 +152,7 @@ const tvWatching = [
         title: "",
         poster: ""
     },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    },
-    {
-        id: "",
-        title: "",
-        poster: ""
-    }
+   
 ]
 
 const handshakeFiveTv = [
