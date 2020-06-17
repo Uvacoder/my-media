9c85c5a5b8950ee48af6236b9c81ccc6d5ad45f8
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-06-17 12:17:27 -0400

    added review

--- src/movieList.js
+++ src/movieList.js
@@ -3,49 +3,57 @@ const moviesWatched = [
         id: "388",
         title: "Inside Man",
         poster: "https://image.tmdb.org/t/p/w200/mf4i9zZVXMmqWsEb9D0OAY7k3t.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     }, 
     {
         id: "44048",
         title: "Unstoppable",
         poster: "https://image.tmdb.org/t/p/w200/uZ1aFZA5qCBUBnmeyNAcip8oRaa.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "449443",
         title: "Den of Thieves",
         poster: "https://image.tmdb.org/t/p/w200/AfybH6GbGFw1F9bcETe2yu25mIE.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "105",
         title: "Back to the Future",
         poster: "https://image.tmdb.org/t/p/w200/7lyBcpYB0Qt8gYhXYaEZUNlNQAv.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     }, 
     {
         id: "68718",
         title: "Django Unchained",
         poster: "https://image.tmdb.org/t/p/w200/7oWY8VDWW7thTzWh3OKYRkWUlD5.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "69729",
         title: "Magnet of Doom",
         poster: "https://image.tmdb.org/t/p/w200/s0lLCnTNqvo10KbEFAc6neoSi7Z.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "466272",
         title: "Once Upon a Time… in Hollywood",
         poster: "https://image.tmdb.org/t/p/w200/8j58iEBw9pOXFD2L0nt0ZXeHviB.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     }, 
     {
         id: "78",
         title: "Blade Runner: The Final Cut",
         poster: "https://image.tmdb.org/t/p/w200/vfzE3pjE5G7G7kcZWrA3fnbZo7V.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "49517",
@@ -57,67 +65,78 @@ const moviesWatched = [
         id: "28387",
         title: "Kicking and Screaming",
         poster: "https://image.tmdb.org/t/p/w200/186brkmaBmopqcM0dPs71tj9BfM.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     }, 
     {
         id: "203",
         title: "Mean Streets",
         poster: "https://image.tmdb.org/t/p/w200/9msfwOeGc9uL1iRRTBdEf15XonC.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "9319",
         title: "The Last Boy Scout",
         poster: "https://image.tmdb.org/t/p/w200/hAejapki8nDEHsdsoffsxETQpjZ.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "10729",
         title: "Quick Change",
         poster: "https://image.tmdb.org/t/p/w200/zH7tQXh1GL28EV31ErPr7Csli4D.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     }, 
     {
         id: "31044",
         title: "My Favorite Year",
         poster: "https://image.tmdb.org/t/p/w200/sQ5TLQ24ivOTqpUDQYd5x5m9Oz8.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "934",
         title: "Rififi",
         poster: "https://image.tmdb.org/t/p/w200/heVdAFNZUxXVmO6jiJcEHCvI5lK.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "26030",
         title: "Bob le Flambeur",
         poster: "https://image.tmdb.org/t/p/w200/zg03eDA4Z7KLU5b1zsgXhNijYWW.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     }, 
     {
         id: "25047",
         title: "Le Deuxième Souffle",
         poster: "https://image.tmdb.org/t/p/w200/kg6c92YKzu0spxILE4EEXQxZNpT.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "5511",
         title: "Le Samouraï",
         poster: "https://image.tmdb.org/t/p/w200/axuBeLVBeXfVZPGg6ph2taWRDFq.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "11657",
         title: "Le Cercle Rouge",
         poster: "https://image.tmdb.org/t/p/w200/3X1EqovVKNpHoYHAhPznWDwQmqB.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     }, 
     {
         id: "42741",
         title: "Un flic",
         poster: "https://image.tmdb.org/t/p/w200/zem0JFFU0AYAlXU0rwHVLPotStq.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
   
 ]
@@ -127,61 +146,71 @@ const moviesWatching = [
         id: "3090",
         title: "The Treasure of the Sierra Madre",
         poster: "https://image.tmdb.org/t/p/w200/qvExr87oqKHvNiFRK5VqDoZrS5D.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "473033",
         title: "Uncut Gems",
         poster: "https://image.tmdb.org/t/p/w200/6XN1vxHc7kUSqNWtaQKN45J5x2v.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "7345",
         title: "There Will Be Blood",
         poster: "https://image.tmdb.org/t/p/w200/fa0RDkAlCec0STeMNAhPaF89q6U.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "21610",
         title: "Nighthawks",
         poster: "https://image.tmdb.org/t/p/w200/W3YNOa27V6POZtE2TPrzmCBP6t.jpg",
-        rating: 2
+        rating: 2,
+        review: ""
     },
     {
         id: "11228",
         title: "Daylight",
         poster: "https://image.tmdb.org/t/p/w200/a4W0QmwlOEKMlvVH4O6ICaN7Kcy.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "419704",
         title: "Ad Astra",
         poster: "https://image.tmdb.org/t/p/w200/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "13580",
         title: "The Spy Who Came in from the Cold",
         poster: "https://image.tmdb.org/t/p/w200/gq4rjEdNO68kQh98yEihWAnJBO8.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "9065",
         title: "Europa",
         poster: "https://image.tmdb.org/t/p/w200/ot4yJ4Hi1pXPaQpejUGtOIcAN18.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "6538",
         title: "Charlie Wilson's War",
         poster: "https://image.tmdb.org/t/p/w200/45FghqcdSYRWK7PsHUInaFPNd8l.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "10021",
         title: "The Secret of My Success",
         poster: "https://image.tmdb.org/t/p/w200/67I1gaW6IA82Ac8lVDpHovDHIz7.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
 ]
 
@@ -190,31 +219,36 @@ const handshakeFiveMovies = [
         id: "289",
         title: "Casablanca",
         poster: "https://image.tmdb.org/t/p/w200/5K7cOHoay2mZusSLezBOY0Qxh8a.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "567",
         title: "Rear Window",
         poster: "https://image.tmdb.org/t/p/w200/ILVF0eJxHMddjxeQhswFtpMtqx.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "389",
         title: "12 Angry Men",
         poster: "https://image.tmdb.org/t/p/w200/7sf9CgJz30aXDvrg7DYYUQ2U91T.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "25364",
         title: "Ace in the Hole",
         poster: "https://image.tmdb.org/t/p/w200/1NwZ6Sx4Eq7eT02abcI7YqfXdJk.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "562",
         title: "Die Hard",
         poster: "https://image.tmdb.org/t/p/w200/aJCpHDC6RoGz7d1Fzayl019xnxX.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     }
 
 ]
--- src/tvList.js
+++ src/tvList.js
@@ -3,121 +3,141 @@ const tvWatched = [
         id: "100698",
         title: "Tiger King: Murder, Mayhem and Madness",
         poster: "https://image.tmdb.org/t/p/w200/pmjYMCnSwndlEpiFZhhOWSWmUsu.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "83631",
         title: "What We Do in the Shadows",
         poster: "https://image.tmdb.org/t/p/w200/aEwa2kqu5XORbmMwdhEHufODcIr.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "93219",
         title: "The Movies That Made Us",
         poster: "https://image.tmdb.org/t/p/w200/kowlD5nELdhoJcQY6J8JZUTeNXN.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "688",
         title: "The West Wing",
         poster: "https://image.tmdb.org/t/p/w200/rMdRbtqJPIO6d71JD7bOwTl8105.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "74204",
         title: "Big Mouth",
         poster: "https://image.tmdb.org/t/p/w200/h6InpGTYbu03b6iVsnniVPqPfIw.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "2316",
         title: "The Office",
         poster: "https://image.tmdb.org/t/p/w200/qWnJzyZhyy74gjpSjIXWmuk0ifX.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "59659",
         title: "Halt and Catch Fire",
         poster: "https://image.tmdb.org/t/p/w200/l4qvJ0lq59wR3ODX42DxBVFGoxx.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "59717",
         title: "Comedians in Cars Getting Coffee",
         poster: "https://image.tmdb.org/t/p/w200/xvQ0jJmrYjxjPMEHP4e8IYIjojF.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "66732",
         title: "Stranger Things",
         poster: "https://image.tmdb.org/t/p/w200/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "2693",
         title: "Extras",
         poster: "https://image.tmdb.org/t/p/w200/zV4Xpdkk6IrGk2mnhrKdtQOHj4I.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "88728",
         title: "I Think You Should Leave with Tim Robinson",
         poster: "https://image.tmdb.org/t/p/w200/m4iNYM0ZaaH10Qzebd9dwSa6Zy6.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "62560",
         title: "Mr. Robot",
         poster: "https://image.tmdb.org/t/p/w200//oKIBhzZzDX07SoE2bOLhq2EE8rf.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "67744",
         title: "Mindhunter",
         poster: "https://image.tmdb.org/t/p/w200/fbKE87mojpIETWepSbD5Qt741fp.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "63351",
         title: "Narcos",
         poster: "https://image.tmdb.org/t/p/w200/rTmal9fDbwh5F0waol2hq35U4ah.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "1436",
         title: "Justified",
         poster: "https://image.tmdb.org/t/p/w200/aVYuGDlOZMCRt89fAnHTYgk3BMr.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "4546",
         title: "Curb Your Enthusiasm",
         poster: "https://image.tmdb.org/t/p/w200/kWQDOnLs5DK0ta8xQZLsaienIHp.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "1621",
         title: "Boardwalk Empire",
         poster: "https://image.tmdb.org/t/p/w200/kL6SqlVPpfAof2nQbh1VxkUuXBQ.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "65495",
         title: "Atlanta",
         poster: "https://image.tmdb.org/t/p/w200/xVmzrYFAtKzqfbweGM8KSgOiEvT.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "15621",
         title: "The Newsroom",
         poster: "https://image.tmdb.org/t/p/w200/oRt7aL1D4WZhqqOLpgh9KGODjQK.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "61859",
         title: "The Night Manager",
         poster: "https://image.tmdb.org/t/p/w200/hdCC5N42cXERbzIn6ED7oqTKtkq.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
 ]
 
@@ -126,61 +146,72 @@ const tvWatching = [
         id: "18347",
         title: "Community",
         poster: "https://image.tmdb.org/t/p/w200/3KUjDt8XY7w2Ku70UE0SECmv1zP.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
+    
     {
         id: "1438",
         title: "The Wire",
         poster: "https://image.tmdb.org/t/p/w200/4lbclFySvugI51fwsyxBTOm4DqK.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "69740",
         title: "Ozark",
         poster: "https://image.tmdb.org/t/p/w200/oy7Peo5iFIt9sNM59lN6csbJeX2.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "1915a",
         title: "The Larry Sanders Show",
         poster: "https://image.tmdb.org/t/p/w200/qMRVylcs6y2naOp7s6RIawELTAL.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "60625",
         title: "Rick and Morty",
         poster: "https://image.tmdb.org/t/p/w200/8kOWDBK6XlPUzckuHDo3wwVRFwt.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "2710",
         title: "It's Always Sunny in Philadelphia",
         poster: "https://image.tmdb.org/t/p/w200/xX3vAWdCb828T48HM9OvvD0p4PC.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "1406",
         title: "Deadwood",
         poster: "https://image.tmdb.org/t/p/w200/4Yp35DVbVOAWkfQUIQ7pbh3u0aN.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "1426",
         title: "Luther",
         poster: "https://image.tmdb.org/t/p/w200/hDxOMX8zzH1FiqKWVBzNaYGBkle.jpg",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {
         id: "73375",
         title: "Tom Clancy's  Jack Ryan",
         poster: "https://image.tmdb.org/t/p/w200/1raUrQr8Y2tSYjTDbk8wfWGKTmX.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {
         id: "60694",
         title: "Last Week Tonight with John Oliver",
         poster: "https://image.tmdb.org/t/p/w200/hUpW9ZakeKpgIzvkck2T59RCHcH.jpg",
-        rating: 3
+        rating: 3,
+        review: ""
     },
    
 ]
@@ -190,31 +221,36 @@ const handshakeFiveTv = [
         id: "1398",
         title: "The Sopranos",
         poster: "https://image.tmdb.org/t/p/w200/6nNZnnUkXcI3DvdrkclulanYXzg.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "1915",
         title: "The Larry Sanders Show",
         poster: "https://image.tmdb.org/t/p/w200/qMRVylcs6y2naOp7s6RIawELTAL.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "1400",
         title: "Seinfeld",
         poster: "https://image.tmdb.org/t/p/w200/aCw8ONfyz3AhngVQa1E2Ss4KSUQ.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "1918",
         title: "The Twilight Zone",
         poster: "https://image.tmdb.org/t/p/w200/s6YY9noG1vPDs59ajkEWXBb1fpk.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "46533",
         title: "The Americans",
         poster: "https://image.tmdb.org/t/p/w200/qB7WPVQnmODg2mZ1xUmPOrCa0wL.jpg",
-        rating: 5
+        rating: 5,
+        review: ""
     }
    
 ]
