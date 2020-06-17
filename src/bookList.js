94420799109038c84e387be98214e71052ec5b60
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-06-17 12:09:12 -0400

    added review item

--- src/bookList.js
+++ src/bookList.js
@@ -2,19 +2,22 @@
     {   id: "21",
         title: "Neuromancer",
         isbn: "0441012035",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {   
         id: "22",
         title: "What It Takes: The Way to the White House",
         isbn: "9780679746492",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {   
         id: "23",
         title: "London Match",
         isbn: "0345332687",
-        rating: 3
+        rating: 3,
+        review: ""
     }
 ]
 
@@ -23,91 +26,111 @@ const booksRead = [
         id: "6",
         title: "Burning Chrome",
         isbn: "9780060539825",
-        rating: 4
+        rating: 4,
+        review: ""
+
     },
     {   
         id: "7",
         title: "Mexico set",
         isbn: "0345314999",
-        rating: 4
+        rating: 4,
+        review: ""
+
     },
     {   
         id: "8",
         title: "Berlin game",
         isbn: "9780345418340",
-        rating: 4
+        rating: 4,
+        review: ""
+
     },
     {   
         id: "9",
         title: "The Score",
         isbn: "0446677736",
-        rating: 4
+        rating: 4,
+        review: ""
+
     },
     {   
         id: "10",
         title: "The Mourner",
         isbn: "9780446677721",
-        rating: 3
+        rating: 3,
+        review: ""
+
     },
     {   
         id: "11",
         title: "The Quiet American",
         isbn: "0140185003",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {   
         id: "12",
         title: "Our Man in Havana",
         isbn: "9780140184938",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {   
         id: "13",
         title: "Factotum",
         isbn: "9780876852637",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {   
         id: "14",
         title: "Post Office",
         isbn: "0061177571",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {   
         id: "15",
         title: "Pulp",
         isbn: "9780876859261",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {   
         id: "16",
         title: "The New Confessions of an Economic Hit Man",
         isbn: "1626566747",
-        rating: 3
+        rating: 3,
+        review: ""
     },
     {   
         id: "17",
         title: "The Outfit",
         isbn: "0446674672",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {   
         id: "18",
         title: "The Man With The Getaway Face",
         isbn: "0446674664",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {   
         id: "19",
         title: "The Hunter",
         isbn: "0226770990",
-        rating: 4
+        rating: 4,
+        review: ""
     },
     {   
         id: "20",
         title: "Dream Team",
         isbn: "0345520491",
-        rating: 4
+        rating: 4,
+        review: ""
     }
     
 ]
@@ -117,7 +140,8 @@ const handshakeFiveBooks = [
         id: "1",
         title: "A Confederacy of Dunces",
         isbn: "0802130208",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {
         id: "2",
@@ -130,19 +154,22 @@ const handshakeFiveBooks = [
         id: "3",
         title: "The Bonfire of the Vanities",
         isbn: "9780553381344",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {   
         id: "4",
         title: "The Guns of August",
         isbn: "0345476093",
-        rating: 5
+        rating: 5,
+        review: ""
     },
     {   
         id: "5",
         title: "Slaughterhouse-Five",
         isbn: "0385333846",
-        rating: 5
+        rating: 5,
+        review: ""
     }
 ]
 
