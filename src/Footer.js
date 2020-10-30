801815d35abeaef2d689025baed3d1efc24e0880
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-10-30 12:57:13 -0400

    website change

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     return (
             <div  className="footer"> 
             
-            <a href="https://scottjellen.me/" target="_blank" rel="noopener noreferrer">
+            <a href="https://scottjellen.com/" target="_blank" rel="noopener noreferrer">
                 <img src="https://s3.us-east-2.amazonaws.com/scottjellen.me.projectlist/sJellenLogo.jpg"
                     alt="logo"
                     className="logo"
